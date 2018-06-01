0c3f3107cf6e510b4bbe67296ccc70f73b72a4d0
Irving Herrera <irving.herreramolina@gmail.com>
2018-06-01 15:49:34 -0500

    iniciando interfaz

--- CrudAspCoreAngular/CrudAspCoreAngular/ClientApp/src/app/app.module.ts
+++ CrudAspCoreAngular/CrudAspCoreAngular/ClientApp/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+
 
 @NgModule({
   declarations: [
@@ -22,6 +24,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    NgbModule.forRoot (),
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
